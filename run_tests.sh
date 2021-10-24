464a592fa49a2c39e59e8a7cd5e3d24522dbad31
edgardleal <edgardleal@gmail.com>
2021-10-24 14:04:57 -0300

    🐞 fix script path for tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,14 @@
-#!/bin/bash
+#! /bin/bash
 #
-# tests.sh
+# run_tests.sh
 # Copyright (C) 2021 edgardleal <edgardleal@MacBook-Air-de-Edgard.local>
 #
 # Distributed under terms of the MIT license.
 #
 
 
+declare -r prev_dir="$(pwd)"
+cd "$1"
+npm run test
+cd "$prev_dir"
 
-echo "$PROJECT_LIST" | tr ',' '\n' | xargs -L 1 ./run_tests.sh
